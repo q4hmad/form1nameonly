a55a1debfa8475429ae33312fd5c0cc6b057813a
Qudsia Ahmad <qudsiaahmad@Aimens-MacBook-Pro.local>
2017-08-18 21:30:42 -0700

    css changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $("#nameForm form").submit(function(event) {
-    var blanks = ["theirName", "theirAge"];
-    blanks.forEach(function(blank){
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    })
-
-    $("#letter").show();
-
+    var theirName = $("#theirName").val();
+    console.log(theirName);
+    var theirAddress = $("#theirAddress").val();
+    console.log(theirAddress);
+    // var blanks = ["theirName", "theirAge"];
+    // blanks.forEach(function(blank){
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // })
+    //
+    // $("#letter").show();
+    //
     event.preventDefault();
   });
 });
